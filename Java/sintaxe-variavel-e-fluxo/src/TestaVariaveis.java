de9ac2e21739b06a314ab8a1302976582f2d99f5
José LR Reis <jreis78pt@gmail.com>
2020-03-07 19:47:23 -0300

    Numeric types

--- Java/sintaxe-variavel-e-fluxo/src/TestaVariaveis.java
+++ Java/sintaxe-variavel-e-fluxo/src/TestaVariaveis.java
@@ -5,13 +5,13 @@ public class TestaVariaveis {
 		// TODO Auto-generated method stub
 		double valor1 = .1;
 		double valor2 = .2;
-		
+
 		System.out.print(valor1 + valor2); // 0.30000000000000004!!!!!
-		
-		//float f = 3.14; // Type mismatch: cannot convert from double to float
+
+		// float f = 3.14; // Type mismatch: cannot convert from double to float
 		float f1 = 3.14f;
-		float f2 = (float)3.14;
-		
+		float f2 = (float) 3.14;
+
 		System.out.print(f1 + f2); // 0.300000000000000046.28 !!!!!
 	}
 
