a4023bce2649392a7658cb2636fd7873e159a429
José LR Reis <jreis78pt@gmail.com>
2020-03-08 10:06:50 -0300

    [Java] Site de diagramação e BigDecimal

--- Java/helloworld/Main.java
+++ Java/helloworld/Main.java
@@ -3,4 +3,8 @@ class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
+
+	/* 
+		www.figma.com
+	 wire .cc
 }
--- Java/sintaxe-variavel-e-fluxo/src/TestaVariaveis.java
+++ Java/sintaxe-variavel-e-fluxo/src/TestaVariaveis.java
@@ -1,3 +1,4 @@
+import java.math.BigDecimal;
 
 public class TestaVariaveis {
 
@@ -6,13 +7,18 @@ public class TestaVariaveis {
 		double valor1 = .1;
 		double valor2 = .2;
 
-		System.out.print(valor1 + valor2); // 0.30000000000000004!!!!!
+		System.out.println(valor1 + valor2); // 0.30000000000000004!!!!!
 
 		// float f = 3.14; // Type mismatch: cannot convert from double to float
 		float f1 = 3.14f;
 		float f2 = (float) 3.14;
 
-		System.out.print(f1 + f2); // 0.300000000000000046.28 !!!!!
+		System.out.println(f1 + f2); // 0.300000000000000046.28 !!!!!
+		
+		BigDecimal bg1 = new BigDecimal(.1);
+		BigDecimal bg2 = new BigDecimal(.2);
+		System.out.println(bg1.add(bg2)); // 0.30000000000000004!!!!!
+		
 	}
 
 }
